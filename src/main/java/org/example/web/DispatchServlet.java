f1021ba7b0c68dd45b406ee8a0608799dbee474d
luis.gomes <luis.gomes@objective.com.br>
2023-12-07 18:15:24 -0300

    - Add request handling by url
    (cherry picked from commit 7fba99463c57db04d03721b71820df68183166ee)

--- src/main/java/org/example/web/DispatchServlet.java
+++ src/main/java/org/example/web/DispatchServlet.java
@@ -4,16 +4,59 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.datastructures.ControllersInstances;
+import org.example.datastructures.ControllersMap;
+import org.example.datastructures.RequestControllerData;
+import org.example.util.LuisLogger;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 
 public class DispatchServlet extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getRequestURL().toString().endsWith("/favicon.ico"))
+            return;
+
+        String uri = request.getRequestURI();
+        String httpMethod = request.getMethod().toUpperCase();
+        String key = httpMethod + uri;
+        RequestControllerData data = ControllersMap.values.get(key);
+        LuisLogger.log(DispatchServlet.class, "URI:"+uri+"("+httpMethod+") - Handler "+data.getControllerMethod());
+        
+        Object result = null;
+        LuisLogger.log(DispatchServlet.class, "Searching for controller instance");
+        try {
+            Object controller = ControllersInstances.instances.get(data.getControllerClass());
+            if (controller == null) {
+                LuisLogger.log(DispatchServlet.class, "Creating new controller instance");
+                controller = Class.forName(data.getControllerClass()).getDeclaredConstructor().newInstance();
+                ControllersInstances.instances.put(data.getControllerClass(), controller);
+            }
+
+            Method controllerMethod = null;
+            for(Method each : controller.getClass().getMethods()){
+                if(each.getName().equals(data.getControllerMethod())) {
+                    controllerMethod = each;
+                    break;
+                }
+            }
+
+            if(controllerMethod != null) {
+                LuisLogger.log(DispatchServlet.class, "Invoking method "+controllerMethod.getName()+" to handle request");
+                result = controllerMethod.invoke(controller);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
         PrintWriter writer = new PrintWriter(response.getWriter());
-        writer.println("<html><h1>Hello Word from " + request.getRequestURI() + "</h1></html>");
+        writer.println(result);
         writer.close();
     }
 }
