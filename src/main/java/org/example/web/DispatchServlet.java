86c5ab945a988bd62d1b9d5dcb0a09ab64efe156
luis.gomes <luis.gomes@objective.com.br>
2023-12-08 06:21:34 -0300

    treating POST requests with request body

--- src/main/java/org/example/web/DispatchServlet.java
+++ src/main/java/org/example/web/DispatchServlet.java
@@ -11,9 +11,12 @@ import org.example.datastructures.ControllersMap;
 import org.example.datastructures.RequestControllerData;
 import org.example.util.LuisLogger;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class DispatchServlet extends HttpServlet {
 
@@ -47,10 +50,21 @@ public class DispatchServlet extends HttpServlet {
                     break;
                 }
             }
-
-            if(controllerMethod != null) {
-                LuisLogger.log(DispatchServlet.class, "Invoking method "+controllerMethod.getName()+" to handle request");
-                result = controllerMethod.invoke(controller);
+            LuisLogger.log(DispatchServlet.class, "Invoking method "+controllerMethod.getName()+" to handle request");
+            if(controllerMethod.getParameterCount() > 0){
+                LuisLogger.log(DispatchServlet.class, "Method "+controllerMethod.getName() + " has parameters");
+                Object arg;
+                Parameter parameter = controllerMethod.getParameters()[0];
+                if(parameter.getAnnotations()[0].annotationType().getSimpleName().equals("LuisBody")){
+                    String body = readBytesFromRequest(request);
+                    LuisLogger.log(DispatchServlet.class, "    Found parameter from request of type "+parameter.getType().getName());
+                    LuisLogger.log(DispatchServlet.class, "    Parameter content: "+body);
+                    arg = gson.fromJson(body, parameter.getType());
+                    result = controllerMethod.invoke(controller, arg);
+                }
+                else {
+                    result = controllerMethod.invoke(controller);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -61,5 +75,15 @@ public class DispatchServlet extends HttpServlet {
 
         writer.println(gson.toJson(result));
         writer.close();
+    }
+
+    private String readBytesFromRequest(HttpServletRequest request) throws IOException {
+        StringBuilder str = new StringBuilder();
+        String line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        while((line = br.readLine()) != null){
+            str.append(line);
+        }
+        return str.toString();
     }
 }
