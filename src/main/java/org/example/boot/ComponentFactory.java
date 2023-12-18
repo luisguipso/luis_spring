da16a533443a3eea2a6443d823b8ebd8f288818d
luis.gomes <luis.gomes@objective.com.br>
2023-12-18 10:43:27 -0300

    Rafactor obtaining parameter types

--- src/main/java/org/example/boot/ComponentFactory.java
+++ src/main/java/org/example/boot/ComponentFactory.java
@@ -16,7 +16,7 @@ import static java.util.Optional.of;
 
 public class ComponentFactory {
 
-    public static Object createComponent(String className){
+    public static Object createComponent(String className) {
         LuisLogger.log(ComponentFactory.class, "Creating new component instance, for: " + className);
         try {
             return invokeComponentConstructors(ClassUtil.getClass(className));
@@ -40,18 +40,26 @@ public class ComponentFactory {
             return constructor.newInstance();
 
         List<String> parametersTypes = getParametersTypes(constructor);
-        List<Object> instantiatedParameters = getParametersInstances(parametersTypes);
-        return constructor.newInstance(instantiatedParameters.toArray());
+        List<Object> parameters = getParametersInstances(parametersTypes);
+        return constructor.newInstance(parameters.toArray());
     }
 
     private static List<String> getParametersTypes(Constructor<?> constructor) {
-        List<String> parametersTypes = new ArrayList<>(Arrays.stream(constructor.getParameterTypes()).map(Class::getName).toList());
-        for (String type : parametersTypes) {
-            String implementationType = ServiceImplementationMap.implementations.get(type);
-            if (!ComponentUtils.isALuisComponent(type) && implementationType != null)
-                parametersTypes.set(parametersTypes.indexOf(type), implementationType);
-        }
-        return parametersTypes;
+        return getRawParamTypes(constructor).stream()
+                .map(ComponentFactory::replaceInterfaceByImplementation)
+                .toList();
+    }
+
+    private static List<String> getRawParamTypes(Constructor<?> constructor) {
+        List<String> paramClassNames = Arrays.stream(constructor.getParameterTypes()).map(Class::getName).toList();
+        return new ArrayList<>(paramClassNames);
+    }
+
+    private static String replaceInterfaceByImplementation(String type) {
+        String implementationType = ServiceImplementationMap.implementations.get(type);
+        if (!ComponentUtils.isALuisComponent(type) && implementationType != null)
+            return implementationType;
+        return type;
     }
 
     private static List<Object> getParametersInstances(List<String> parametersTypes) {
