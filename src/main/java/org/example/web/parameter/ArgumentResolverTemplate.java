1aa8a3eb80f05e0b29fe19790e88498e755e7247
Luis Guilherme Gomes <luisguilherme_pso@hotmail.com>
2024-03-01 23:30:44 -0300

    Refactor ArgumentResolverTemplate to use the functional style.

--- src/main/java/org/example/web/parameter/ArgumentResolverTemplate.java
+++ src/main/java/org/example/web/parameter/ArgumentResolverTemplate.java
@@ -4,12 +4,10 @@ import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.regex.Pattern;
 import org.example.util.LuisLogger;
-import static java.util.Optional.of;
 
 public interface ArgumentResolverTemplate {
 
@@ -20,32 +18,27 @@ public interface ArgumentResolverTemplate {
     String ERROR_WHEN_READING_BODY_FROM_REQUEST_MSG = "Error when reading body from request to: %s";
     String REQUEST_BODY_FOR_REQUEST_MUST_NOT_BE_EMPTY_MSG = "Request body for request uri: '%s' must not be empty.";
     Pattern PATH_VARIABLE_REGEX_PATTERN = Pattern.compile("[{}]");
+    record ParamArg(Parameter param, String arg){}
 
     default List<Object> resolveMethodParameters(HttpServletRequest request, Method controllerMethod, String methodMappedUri) {
-        List<Object> arguments = new ArrayList<>();
-        for (Parameter parameter : controllerMethod.getParameters()) {
-            Optional<String> argument = Optional.empty();
-            if (isApplicable(parameter)) {
-                argument = of(getArgument(request, parameter, methodMappedUri));
-            }
-            if (argument.isPresent()) {
-                logParameterFound(parameter, argument.get());
-                arguments.add(getUnmarshalledArgument(parameter, argument.get()));
-            }
-        }
-        return arguments;
+        return Arrays.stream(controllerMethod.getParameters())
+                .filter(this::isApplicable)
+                .map(parameter -> new ParamArg(parameter, getArgument(request, parameter, methodMappedUri)))
+                .peek(this::logParameterFound)
+                .map(ArgumentResolverTemplate::getUnmarshalledArgument)
+                .toList();
     }
 
-    private static Object getUnmarshalledArgument(Parameter parameter, String argument) {
-        return new Gson().fromJson(argument, parameter.getType());
+    private static Object getUnmarshalledArgument(ParamArg paramArg) {
+        return new Gson().fromJson(paramArg.arg(), paramArg.param().getType());
     }
 
     boolean isApplicable(Parameter parameter);
 
     String getArgument(HttpServletRequest request, Parameter parameter, String methodMapperUri);
 
-    private void logParameterFound(Parameter parameter, String argument) {
-        LuisLogger.log(getClass(), String.format(PARAMETER_FOUND_FROM_REQ_TYPE_MSG, parameter.getType().getName()));
-        LuisLogger.log(getClass(), String.format(PARAMETER_CONTENT_MSG, argument));
+    private void logParameterFound(ParamArg paramArg) {
+        LuisLogger.log(getClass(), String.format(PARAMETER_FOUND_FROM_REQ_TYPE_MSG, paramArg.param().getType().getName()));
+        LuisLogger.log(getClass(), String.format(PARAMETER_CONTENT_MSG, paramArg.arg()));
     }
 }
